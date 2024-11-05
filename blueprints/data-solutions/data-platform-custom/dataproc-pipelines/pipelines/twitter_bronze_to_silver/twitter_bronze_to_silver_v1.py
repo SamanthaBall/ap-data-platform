17e81a3b9c6f9ac3c7fe4ef1433009d70a040e21
Sam_Ball <samanthab@synthesis.co.za>
2024-11-05 15:12:42 +0200

    Clean up bronze to silver pipeline

--- blueprints/data-solutions/data-platform-custom/dataproc-pipelines/pipelines/twitter_bronze_to_silver/twitter_bronze_to_silver_v1.py
+++ blueprints/data-solutions/data-platform-custom/dataproc-pipelines/pipelines/twitter_bronze_to_silver/twitter_bronze_to_silver_v1.py
@@ -24,32 +24,3 @@ spark.stop()
 
 
 
-# # Group 1: Data Cleaning
-# # Clean text by removing URLs and special characters
-# cleaned_tweets_df = tweets_df.withColumn(
-#     'cleaned_text', 
-#     regexp_replace(col('text'), r'http\S+|[^a-zA-Z\s]', '')
-# )
-
-# # Filter out retweets
-# cleaned_tweets_df = cleaned_tweets_df.filter(~col('is_retweet'))
-
-# # Group 2: Data Enrichment
-# # Example: Add geolocation data (this could be done by calling a geocoding function)
-# enriched_tweets_df = cleaned_tweets_df.withColumn(
-#     'location', when(col('location').isNull(), 'Unknown').otherwise(col('location'))
-# )
-
-# # Example: Enrich user profile with a field for whether the user is verified
-# enriched_tweets_df = enriched_tweets_df.withColumn(
-#     'is_verified', when(col('user.verified') == True, 'Yes').otherwise('No')
-# )
-
-# # Group 3: Data Transformation
-# # Example: Flatten nested fields for easier analysis
-# final_tweets_df = enriched_tweets_df.select(
-#     col('cleaned_text'),
-#     col('user.name').alias('user_name'),
-#     col('location'),
-#     col('is_verified')
-# )
